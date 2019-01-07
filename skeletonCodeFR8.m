ad839c5e0660e455a40d26b6f7b83a97a836f96c
yilmazduygu <duygu.yilmaz@rutgers.edu>
2019-01-07 00:09:46 -0500

    Made changes according to Issues #1
    There are parts I couldn't fix. Commented them in the code.

--- skeletonCodeFR8.m
+++ skeletonCodeFR8.m
@@ -9,7 +9,6 @@ format compact;
 
 %% Process a sequence of files
 % 
-
 % Specify the folder 
 % myFolder = 'C:\Users\MenaLab\\Desktop\Data files';
 myFolder = '/Users/Duygu/Google Drive/Sem III/Scientific Programming in Matlab/my codes for the course/Final project/Data files';
@@ -26,12 +25,15 @@ allFiles = dir(filePattern);
 for ii = 1 : length(allFiles)
   baseFileName = allFiles(ii).name;
   fullFileName = fullfile(myFolder, baseFileName);
-  % Lets user know of what the script's doing
-  fprintf(1, 'Now reading %s\n', baseFileName);
- 
-  data(ii) = readFR8txt(fullFileName); 
+  % Lets user know of what the script is doing. 
+  fprintf(1, '%d. Now reading %s\n', ii, baseFileName);
+  data(ii) = readFR8txt(fullFileName);  % I don't know how to pre-allocate memory for 'data', so I cannot fix the warning Matlab gives here
 end
-fprintf(1, 'Finished reading all files in %s\n', myFolder);
+if ii ~= length(allFiles)
+    error('Problem with reading all the files in %s\n', myFolder);
+else
+    fprintf(1, 'Finished reading all files in %s\n', myFolder);
+end
 save 'data.mat' data
 
 %% Make the figure
@@ -40,7 +42,7 @@ save 'data.mat' data
 %% Function to read each data file
 function S = readFR8txt(filename)
 
-%% Open text file, give error if not possible
+% Open text file, give error if not possible
 fid = fopen(filename);
 if fid<0
     error('Error opening the file %s.\n', filename);
@@ -48,12 +50,11 @@ end
 
 % Set up the struct S, assign default values to each field
 S = struct;
-S.name = '';
-S.date = NaT;
 S.animal = NaN;
+S.date = NaT;
 S.group = NaN;
 S.box = NaN;
-S.time = NaT;
+S.duration = NaT;
 S.program = '';
 S.numPress = NaN;
 S.numReward = NaN;
@@ -61,11 +62,11 @@ S.presses = NaN;
 S.rewards = NaN;
 S.headEntries = NaN;
 S.comments = '';
-
+S.name = '';
 % Read header info, write each bit to its corresponding field in S
 while true
     line = fgetl(fid);
-    if isempty(line) || length(line) == 0;continue;end % Skip empty lines
+    if isempty(line) || length(line) == 0;continue;end % Skip empty lines: I cannot fix the warning Matlab gives here, because for my files, with fgetl, isempty function does not work, only length does
     if line(1) == 'A';break;end % Exit the loop to get the arrays in the next step
     tagValue = strsplit(line,': '); 
     tag = tagValue{1}; 
@@ -79,13 +80,16 @@ while true
             S.name = char(value);
         case 'START DATE'
             % Found the session date
-            S.date = datetime(value, 'InputFormat', 'MM/dd/yy'); 
+            recDate = char(value); % for later use
         case 'END DATE'
             % Found the date session ended, not important, continue
             continue;
         case 'SUBJECT'
             % Found the animal ID #
             S.animal = str2double(value);
+            if ~(str2double(S.name(42:46)) == S.animal)
+                warning('Animal ID possibly read incorrectly');
+            end
         case 'EXPERIMENT'
             % experiment name, not important, continue
             continue;
@@ -97,102 +101,91 @@ while true
             S.box = str2double(value); 
         case 'START TIME'
             % Found the time session has started
-            S.time = datetime(value, 'InputFormat', 'HH:mm:ss'); % here the 
-                % datetime fxn adds today's date before the time info it pulls
+            recTime = char(value);
+            S.date= datetime([recDate, ' ', recTime], 'InputFormat', 'MM/dd/yy HH:mm:ss');
         case 'END TIME'
-            continue;
+            % Found the time session has ended, use this to find out
+            % session duration
+            recTimeEnd = char(value);
+            endTime = datetime([recDate, ' ', recTimeEnd], 'InputFormat', 'MM/dd/yy HH:mm:ss');
+            S.duration = endTime - S.date;
+            if S.duration > minutes(60) % Sanity check
+                warning('Session duration longer than an hour for this one');
+            end
         case 'MSN'
+            % Found the program run that day
             S.program = char(value);
             if strcmp(S.program, 'FR8_Day4_NKG') || strcmp(S.program,'FR8_Day3_NKG') || strcmp(S.program,'FR8_Day2_NKG')
                 return; % I don't want the data for earlier stages of training
             end
         case 'F'
+            % Irrelevant info about MED-PC
             continue;
         case 'L'
+            % Found the total number of presses the animal did
             S.numPress = str2double(value);
         case 'R'
+            % Found the total number of rewards the animal got
             S.numReward = str2double(value);
         otherwise
             error(['Found an unknown tag ' tag]);
     end
 end
 % Now fid should be right below the line "A: "
+if ~(line(1)=='A')
+    error('Cursor is at an unintended line');
+end
 line = fgetl(fid); % irrelevant info on this line, do not store
 
 % Read Lever Press Array (C):
 line = fgetl(fid); % next line, 'C:'
-counter = 0;
-temp = {};
 if line(1) == 'C' % fid is at lever press array
-    while true
-        line = fgetl(fid); % next line, where the data starts
-        if line(1) == 'D';break;end 
-        counter = counter + 1; % counts the lines it reads
-        tagValue = strsplit(line,': '); 
-        tag = tagValue{1}; 
-        value = tagValue{2};
-        A = sscanf(value, '%f');
-        temp{counter} = A';
-    end   
-    if ~(counter== ceil(S.numPress/5)) % sanity check
-        warning('lever press array (C) in %s possibly read incorrectly', filename);
-    end
-    S.presses = cat(2, temp{:});
+    S.presses = readArray('C', 'D');
+else
+    error('Cursor is at an unintended line');
 end
 
 % Read Rewards Array (D):
-counter = 0;
-temp = {};
 if line(1) == 'D' % fid is at rewards array
-    while true
-        line = fgetl(fid); % next line, where the data starts
-        if line(1) == 'E';break;end 
-        counter = counter + 1; % counts the lines it reads
-        tagValue = strsplit(line,': '); 
-        tag = tagValue{1}; 
-        value = tagValue{2};
-        A = sscanf(value, '%f');
-        temp{counter} = A';
-    end   
-    if ~(counter== ceil(S.numReward/5)) % sanity check
-        warning('rewards array (D) in %s possibly read incorrectly', filename);
-    end
-    S.rewards = cell2mat(temp);
+    S.rewards = readArray('D','E');
+else
+    error('Cursor is at an unintended line');
 end
 
 % Read Head Entries Array (E):
+if line(1) == 'E' % fid is at head entry array
+    [S.headEntries, S.comments] = readArray('E', 'X');
+end
+
+
+function [cellArray, comment] = readArray(whichArray, untilWhere)
+allArrays = {'C', 'D', 'E'};
+if ~ismember(whichArray, allArrays)
+    error('Local function called wrong');
+end
+
 counter = 0;
 temp = {};
-if line(1) == 'E' % fid is at head entry array
-    while true
-        line = fgetl(fid); % next line, where the data starts   
-        if line == -1;break;end 
+comment = '';
+while true
+        line = fgetl(fid); % next line, where the data starts
+        if line(1) == untilWhere;break;end
         if line(1) == '\'
-            S.comments = line(2:end);
+            comment = line(2:end);
             continue;
         end
-        counter = counter + 1; 
-        tagValue = strsplit(line,': '); 
-        tag = tagValue{1}; 
-        value = tagValue{2};
-        A = sscanf(value, '%f');
-        temp{counter} = A';
-    end   
-    % I don't have the total num of head entries info on the file, so a
-    % sanity check is not possible for this one
-    S.headEntries = cell2mat(temp); 
+        if line == -1;break;end
+        counter = counter + 1; % counts the lines it reads
+        values = extractAfter(line, ': ');
+        A = sscanf(values, '%f');
+        temp{counter} = A'; %I probably couldn't understand what you meant, because I couldn't get rid of this transpose, nor the str2num worked.
+end
+cellArray = cat(2, temp{:});
+% I meant to use the values I read to S.numPress and S.numReward in this
+% nested function. But apperantly I cannot pass variables I created above
+% to the nested function?
 end
 
 % Close the file
 fclose(fid);
-
 end
-
-%% problems:
-
-% #1. datetime function on line 100 does a funny thing: even though the
-% format i specify is HH:mm:ss, it adds todat's date in front of the time
-% and writes to the data struct in that form. I probably wont be using that
-% info, but still. Am I calling the function wrong again?
-% #2. lines 141, 161, and 184: I am not sure which one to pick. Both
-% versions seem to work fine, though?
